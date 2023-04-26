f20a6c577a3dde66a5f4c05f5a348fb8d6025bf0
JerrySippy <104254392+JerrySippy@users.noreply.github.com>
2023-04-26 21:10:46 +0300

    Add files via upload

---
+++ lets_chat_room.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDuIUCVm41Lnj7K4wx91LIkqgyRcbX031M",
+    authDomain: "letschat-1fb94.firebaseapp.com",
+    databaseURL: "https://letschat-1fb94-default-rtdb.firebaseio.com",
+    projectId: "letschat-1fb94",
+    storageBucket: "letschat-1fb94.appspot.com",
+    messagingSenderId: "592297555832",
+    appId: "1:592297555832:web:d6c64f53c819f717b43a8d"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
